77378db9ff65ce56c8e18f2c35e715cb6df10a26
fatimasif <104876521+fatimasif@users.noreply.github.com>
2023-02-16 22:48:22 -0500

    Add files via upload

---
+++ reverseorder.h
@@ -0,0 +1,21 @@
+/*
+Author: Fatima Asif
+Course: CSCI-135
+Instructor: M. Zamansky
+Assignment: reverseorder.h, lab-03
+*/
+
+// TASK D
+
+#pragma once
+#ifndef REVERSEORDER_H
+#define REVERSEORDER_H
+
+#include <string>
+#include "reservoir.h"
+
+void reverse_order(std::string, std::string);
+
+#endif
+
+
